04c435d9c42d7fbf741926e257c8fa46bbbeaa04
Cyrus Xu <xuzengwei06@gmail.com>
2021-09-01 20:47:00 +1000

    add user block

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React, { useEffect, useState } from 'react';
-import './style/App.css';
+import { Form, Input, Button } from 'antd';
+import './style/App.less';
+import './style/loading.less';
 
 const App = () => {
   return (
     <div className='container'>
       <h1 className='title'>weather forecasting web application</h1>
-      <div className='user'>
+      <div className='search-block'>
+        <Form
+          name="basic"
+          autoComplete="off"
+        >
+          <Form.Item
+            label="Location"
+            name="location"
+            rules={[{ required: true, message: 'Please input location!' }]}
+          >
+            <Input />
+          </Form.Item>
 
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
       </div>
     </div>
   )
