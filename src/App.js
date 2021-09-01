4ed4e3f68ef7bc0d585304a25ef8c4dabfc9bbb2
Cyrus Xu <xuzengwei06@gmail.com>
2021-09-01 20:28:16 +1000

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from 'react';
+import './style/App.css';
+
+const App = () => {
+  return (
+    <div className='container'>
+      <h1 className='title'>weather forecasting web application</h1>
+      <div className='user'>
+
+      </div>
+    </div>
+  )
+}
+
+
+export default App;
